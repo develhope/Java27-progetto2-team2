7c1015c2194fd1411a6048c9fdb06d9441b82c76
Fabio R <fabioraffa01@gmail.com>
2024-07-30 17:46:14 +0200

    - Creata classe Enum x differenziare gestore magazzino da cliente - Modificata classe utente per uso enum, creati metodi di controllo ruolo e controlli sugli altri metodi - Modificato GetCarrelloUtente per tornare la lista dei prodotti

--- src/Utente.java
+++ src/Utente.java
@@ -8,14 +8,16 @@ public class Utente {
     private final String id;
     private String password; //password potrebbe essere l'unico dato modificabile con metodo apposito?
     private final Carrello carrelloUtente; // Carrello associato all'utente
+    private final RuoloUtente ruolo;
 
 // Costruttore
-    public Utente(String nome, String cognome, String id, String password) {
+    public Utente(String nome, String cognome, String id, String password, RuoloUtente ruolo) {
         this.nome = nome;
         this.cognome = cognome;
         this.id = id;
         this.password = password;
-        this.carrelloUtente = new Carrello(); // Non è necessario passare il Magazzino// Non è necessario passare il Magazzino
+        this.carrelloUtente = new Carrello(); // Non è necessario passare il Magazzino
+        this.ruolo = ruolo;
     }
 
 // Controllo che non ci siano dati null o mancanti, altrimenti lancio eccezione
@@ -41,9 +43,11 @@ public class Utente {
         return id;
     }
 
-    public Carrello getCarrelloUtente() {
-        return carrelloUtente;
+    //30.07 Modificato in modo che restituisca lista prodotti carrello dell'utente
+    public List<Prodotto> getCarrelloUtente() {
+        return carrelloUtente.getContentCarrello();
     }
+
 // Gestione apposita per dato password
     // Non viene mostrata direttamente la password
     public boolean verificaPassword(String password) {
@@ -58,15 +62,30 @@ public class Utente {
         this.password = nuovaPassword;
     }
 
+//30.07 Metodi per controllare ruolo dell'utente ---------------------------------------------------
+    public boolean isGestore() {
+        return this.ruolo == RuoloUtente.GESTORE;
+    }
+    public boolean isCliente() {
+        return this.ruolo == RuoloUtente.CLIENTE;
+    }
+
 // Metodi per la gestione del carrello -------------------------------------------------------------
+//30.07 con controllo se Cliente
     /**
      * Aggiunge un prodotto al carrello dell'utente.
      * @param prodotto Prodotto da aggiungere
      */
     public void aggiungiProdottoAlCarrello(Prodotto prodotto) {
-        boolean aggiunto = carrelloUtente.aggiungiProdotto(prodotto);
-        if (!aggiunto) {
-            System.out.println("Il prodotto è già presente nel carrello.");
+        if (isCliente()) {
+            boolean aggiunto = carrelloUtente.aggiungiProdotto(prodotto);
+            if (!aggiunto) {
+                System.out.println("Il prodotto è già presente nel carrello.");
+            } else {
+                System.out.println("Prodotto aggiunto al carrello con successo.");
+            }
+        } else {
+            System.out.println("Operazione non permessa. Solo i clienti possono aggiungere prodotti al carrello.");
         }
     }
 
@@ -75,21 +94,29 @@ public class Utente {
      * @param idProdotto ID del prodotto da rimuovere
      */
     public void rimuoviProdottoDalCarrello(int idProdotto) {
-        try {
-            Prodotto prodottoRimosso = carrelloUtente.rimuoviProdotto(idProdotto);
-            System.out.println("Prodotto rimosso dal carrello: " + prodottoRimosso);
-        } catch (ProductDoesntExistException e) {
-            System.out.println("Errore: " + e.getMessage());
+        if (isCliente()) {
+            try {
+                Prodotto prodottoRimosso = carrelloUtente.rimuoviProdotto(idProdotto);
+                System.out.println("Prodotto rimosso dal carrello: " + prodottoRimosso);
+            } catch (ProductDoesntExistException e) {
+                System.out.println("Errore: " + e.getMessage());
+            }
+        } else {
+            System.out.println("Operazione non permessa. Solo i clienti possono rimuovere prodotti dal carrello.");
         }
     }
 
     // Metodo per visualizzare il contenuto del carrello
     public void visualizzaCarrello() {
-        List<Prodotto> contenuto = carrelloUtente.getContentCarrello();
-        if (contenuto.isEmpty()) {
-            System.out.println("Il carrello è vuoto.");
+        if (isCliente()) {
+            List<Prodotto> contenuto = carrelloUtente.getContentCarrello();
+            if (contenuto.isEmpty()) {
+                System.out.println("Il carrello è vuoto.");
+            } else {
+                contenuto.forEach(System.out::println);
+            }
         } else {
-            contenuto.forEach(System.out::println);
+            System.out.println("Operazione non permessa. Solo i clienti possono visualizzare il carrello.");
         }
     }
 
@@ -98,17 +125,26 @@ public class Utente {
      * @return Totale del carrello
      */
     public double calcolaTotaleCarrello() {
-        return carrelloUtente.calcolaTotale();
+        if (isCliente()) {
+            return carrelloUtente.calcolaTotale();
+        } else {
+            System.out.println("Operazione non permessa. Solo i clienti possono calcolare il totale del carrello.");
+            return 0;
+        }
     }
 
     // Metodo con messaggio di acquisto finalizzato e che svuoti il carrello
     public void finalizzaAcquisto() {
-        double totale = calcolaTotaleCarrello();
-        if (totale > 0) {
-            carrelloUtente.svuotaCarrello();
-            System.out.println("Acquisto finalizzato con successo! Totale spesa: €" + totale + ". Grazie " + nome + " " + cognome + ".");
+        if (isCliente()) {
+            double totale = calcolaTotaleCarrello();
+            if (totale > 0) {
+                carrelloUtente.svuotaCarrello();
+                System.out.println("Acquisto finalizzato con successo! Totale spesa: €" + totale + ". Grazie " + nome + " " + cognome + ".");
+            } else {
+                System.out.println("Il carrello è vuoto. Impossibile finalizzare l'acquisto.");
+            }
         } else {
-            System.out.println("Il carrello è vuoto. Impossibile finalizzare l'acquisto.");
+            System.out.println("Operazione non permessa. Solo i clienti possono finalizzare l'acquisto.");
         }
     }
 }
